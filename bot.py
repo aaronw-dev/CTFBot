a2b4a8783de237267c4c4f73b540aa915b709422
AW_Dev <riptidegamesceo@gmail.com>
2023-08-26 21:36:33 -0700

    - changed command arguments - added URL variables - users can change how many events they recieve - dump calendar information to output.json for debugging - sort upcoming events by their start date - shows duration of events that give out that information - added on message event for the future

--- bot.py
+++ bot.py
@@ -4,35 +4,73 @@ from datetime import datetime
 import calendar
 import discord
 from discord import app_commands
+import pytz
+from dateutil.relativedelta import relativedelta
+from math import *
 
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-rssurl = "https://clients6.google.com/calendar/v3/calendars/ctftime@gmail.com/events?calendarId=ctftime%40gmail.com&singleEvents=true&timeZone=Africa%2FAbidjan&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2023-07-31T00%3A00%3A00Z&timeMax=2023-09-04T00%3A00%3A00Z&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"
 webhookurl = "https://discord.com/api/webhooks/1145053865599828000/qx5snul2GwFrSNz3-qvEupR-mGgKmrCr2L2U9rfstXarp_x2QaaioR1olmbuUqjkst9w"
 
 @tree.command(name="getctf", description="Find upcoming CTFs")
 #variable structure: VARIABLENAME: TYPE = DEFAULTVALUE
-async def getctf(interaction):
+async def getctf(interaction, amount: app_commands.Range[int, 5, 20] = 10):
+    maxresults = amount + 1
+    timemin = datetime.now().strftime("%Y-%m-%d")
+    timemax = (datetime.now() + relativedelta(years=1)).strftime("%Y-%m-%d")
+    rssurl = f"https://clients6.google.com/calendar/v3/calendars/ctftime@gmail.com/events?calendarId=ctftime%40gmail.com&singleEvents=true&timeZone=Africa%2FAbidjan&maxAttendees=1&maxResults={maxresults}&sanitizeHtml=true&timeMin={timemin}T00%3A00%3A00Z&timeMax={timemax}T00%3A00%3A00Z&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"
     res = requests.get(rssurl)
+    
     rescontent = res.content.decode('utf8')  # .replace("'", '"')
 
     responsejson = json.loads(rescontent)
-    names = []
+    with open("output.json", "w") as file:
+        json.dump(responsejson, file, indent=4)
     finalmessage = '''
     > ## Here are some upcoming CTFs:
     '''
-    for item in responsejson["items"]:
-        names.append(item)
+    upcomingevents = responsejson["items"]
+    upcomingevents = sorted(upcomingevents, key=lambda x: x["start"]["dateTime"])
+    names = []
+    messagewidth = 60
+    for item in upcomingevents:
+        eventName = item["summary"]
         eventTime = datetime.fromisoformat(item["start"]["dateTime"])
-        finalmessage += "> *" + item["summary"] + "* on " + str(
-            eventTime.day) + " " + calendar.month_name[eventTime.month] + " " + str(eventTime.year) + "\n"
+        eventTimezone = item["start"]["timeZone"] #Format: UTC
+        eventDuration = datetime.fromisoformat(item["end"]["dateTime"]) - datetime.fromisoformat(item["start"]["dateTime"])
+        durationDays = eventDuration.days
+        durationHours = floor(eventDuration.seconds / 3600)
+        durationMinutes = floor(((eventDuration.seconds / 3600) - durationHours) * 60)
+        durationSeconds = floor((((eventDuration.seconds / 3600) - durationHours) * 60 - durationMinutes) * 60)
+        now = datetime.now()
+        now = pytz.UTC.localize(now) 
+        if(eventTime < now):
+            continue
+        eventLink = item["description"][-32:-2]
+        names.append(item)
+        finalmessage += "> " + "[" + item["summary"] + "](<" + eventLink + ">)" + " on " + "<t:" + str(calendar.timegm(eventTime.timetuple())) + ":D>"
+        finalmessage += (" duration: " + str(durationHours) + " hour" + ("s" if durationHours != 1 else "") + " " + ((str(durationMinutes) + " minute" + ("s" if durationHours != 1 else "")) if durationMinutes != 0 else "")) if durationHours != 0 else "" 
+        finalmessage += "\n"
     finalmessage += "\n"
     body = {
         "content": finalmessage
     }
+    
     await interaction.response.send_message(body["content"], ephemeral=False)
+'''
+Code to post a webhook
+requests.post(
+    webhookurl,
+    json=body)
+'''
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    print(message.author.display_name + ": " + message.content)
+    #await message.channel.send("balls")
 
 @client.event
 async def on_ready():
